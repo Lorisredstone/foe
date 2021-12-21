92f19e6b2a219ebe72ba73e9cea681eedf66a70d
Loris <monestierloris11@gmail.com>
2021-12-21 20:49:47 +0100

    update+config

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from asyncio import sleep
+import asyncio
 import discord
 import json
 
 config_private = json.load(open('config/private.json'))
+config = json.load(open('config/config.json'))
 
 bot = discord.Client()
 
 @bot.event
 async def on_ready():
-    for a in ["par pf4", "$help"]:
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=a, type=3))
-        sleep(5)
+    while True:
+        for status in config["status"]["liste_status"]:
+            await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=status, type=3))
+            await asyncio.sleep(config["status"]["sleep_time"])
 
 @bot.event
 async def on_message(message):
     if message.content == "$ping":
         await message.channel.send("pong")
-        
+    if message.content == "$help":
+        await message.channel.send("Voici la liste des commandes: $ping, $help")
         
 bot.run(config_private["discord_token"])
