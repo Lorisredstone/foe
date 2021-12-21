2dff1b592e37cd76507e59498e3a54a6162e99ff
Loris <monestierloris11@gmail.com>
2021-12-21 20:30:24 +0100

    update

---
+++ main.py
@@ -0,0 +1,22 @@
+from asyncio import sleep
+from token import token
+import discord
+import json
+
+config_private = json.load(open('config/private.json'))
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    for a in ["par pf4", "$help"]:
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=a, type=3))
+        sleep(5)
+
+@bot.event
+async def on_message(message):
+    if message.content == "$ping":
+        await message.channel.send("pong")
+        
+        
+bot.run(config_private["discord_token"])
