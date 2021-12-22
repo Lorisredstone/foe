1a2d796a16871cb968d006290fe18987822bf110
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-22 12:56:39 +0100

    Update gm.py

--- mod/gm.py
+++ mod/gm.py
@@ -8,9 +8,27 @@ def get_batiment_info(batiment, lvl):
 
 async def gm(message, color):
     if message.content == "$gm help":
-        embed = discord.Embed(color=color, title="Commandes $gm", description="**$gm list**: liste les GM\n**$gm info *<nom>* *<lvl>***: Affiche des informations sur le GM")
+        embed = discord.Embed(color=color, title="Commandes $gm", description="**$gm list**: liste les GM\n**$gm info *<id>* *<lvl>***: Affiche des informations sur le GM")
         await message.channel.send(embed=embed)
+
     elif message.content == "$gm list":
-        liste = ", ".join([f"[{traduction.GMname[b][0]}] {traduction.GMname[b][1]}" for b in traduction.GMname.keys()])
+        liste = ", ".join([f"[{traduction.GMname[b][0]}] *{traduction.GMname[b][1]}*" for b in traduction.GMname.keys()])
         embed = discord.Embed(color=color, title="Liste des GM", description=liste)
         await message.channel.send(embed=embed)
+    
+    elif message.content.startswith("$gm info"):
+        try:
+            GMdico = int(message.content.split(" ")[2])
+            GMlvl = message.content.split(" ")[3]
+            for b in traduction.GMname.keys():
+                if GMdico == traduction.GMname[b][0]:
+                    batiment = b
+            batiment_info = get_batiment_info(traduction.GM_ID[batiment], GMlvl)
+            embed = discord.Embed(color=color, title=f"{traduction.GMname[batiment][1]}", description=f"air: {batiment_info['era']}\nPF lvl{GMlvl}: {batiment_info['total_fp']}\nproduit:")
+            print(batiment_info)
+            embed.set_thumbnail(url=batiment_info["image"])
+            for k in batiment_info["rewards"].keys():
+                embed.add_field(name=k.replace("_", " "), value=batiment_info["rewards"][k])
+            await message.channel.send(embed=embed)
+        except Exception as e:
+            await message.channel.send(f"oups une erreur est survenue:\n{e}")
