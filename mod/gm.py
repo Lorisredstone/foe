ab5f9b65ce7098a9cd541441eebe89b4ab68dde4
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-22 12:19:29 +0100

    b

--- mod/gm.py
+++ mod/gm.py
@@ -11,6 +11,6 @@ async def gm(message, color):
         embed = discord.Embed(color=color, title="Commandes $gm", description="**$gm list**: liste les GM\n**$gm info *<nom>* *<lvl>***: Affiche des informations sur le GM")
         await message.channel.send(embed=embed)
     elif message.content == "$gm list":
-        liste = ", ".join([traduction.GMname[b] for b in traduction.GMname.keys()])
+        liste = ", ".join([f"[{traduction.GMname[b][0]}] {traduction.GMname[b][1]}" for b in traduction.GMname.keys()])
         embed = discord.Embed(color=color, title="Liste des GM", description=liste)
         await message.channel.send(embed=embed)
